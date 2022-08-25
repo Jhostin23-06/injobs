f1892be3fa8ef8503f477b357b86c41b70f516df
Jhostin23-06 <bravojhostin232001@gmail.com>
2022-08-24 20:54:26 -0500

    Procfile y htaccess7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        @livewireStyles
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
         @stack('styles')
         <link rel="shortcut icon" href="{{asset('img/job.png')}}" type="png">
-        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
