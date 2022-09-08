367ab97b1758b56766db622e5a3c3e2f90908b7c
Jhostin23-06 <bravojhostin232001@gmail.com>
2022-09-07 20:02:37 -0500

    Composer 13

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -5,8 +5,7 @@
 
         <ul class="mt-3 list-none text-sm space-y-2">
             @foreach ($errors->all() as $error)
-                <li class="bg-red-100 border-l-4 border-red-600 text-red-600 font-bold p-3">{{ $error 
-                }}</li>
+                <li class="bg-red-100 border-l-4 border-red-600 text-red-600 font-bold p-3">{{ $error }}</li>
             @endforeach
         </ul>
     </div>
