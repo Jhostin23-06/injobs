48c20e95b7b5f1b4127f389e7ed4747f97aa7539
Jhostin23-06 <76470940+Jhostin23-06@users.noreply.github.com>
2022-12-04 21:31:05 -0500

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -34,12 +34,12 @@ Route::middleware('guest')->group(function () {
                 ->name('password.update');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['signed', 'throttle:6,1', 'verified'])
+                ->middleware(['signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
